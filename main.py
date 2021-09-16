68d688edd629bd137b8323cbde608f3adc7bd760
lasseb <lbenninga@schubergphilis.com>
2021-09-16 12:30:50 +0200

    Add FIRE for cli

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import os
+import fire
 
 from makelaarsland.crawl import MakelaarslandCrawler
 
-if __name__ == '__main__':
+
+def crawl_makelaarsland():
     username = os.getenv('MAKELAARSLAND_USERNAME', '')
     password = os.getenv('MAKELAARSLAND_PASSWORD', '')
     listing_id = os.getenv('MAKELAARSLAND_LISTING_ID', '')
 
     crawler = MakelaarslandCrawler(username, password, listing_id)
     crawler.crawl_listings()
+
+
+def crawl_walterliving():
+    pass
+
+
+if __name__ == 'main':
+    fire.Fire({
+        'makelaarsland': crawl_makelaarsland,
+        'walterliving': crawl_walterliving,
+    })
