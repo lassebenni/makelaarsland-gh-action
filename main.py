9d4e16cf86030953cf38c794cea752e81e065118
Lasse Benninga <lassebenninga@godatadriven.com>
2022-01-08 21:04:57 +0100

    Fix hypotheek url

--- main.py
+++ main.py
@@ -6,22 +6,21 @@ from src.walter.walterliving import WalterLiving
 
 
 def crawl_makelaarsland(limit: int = 0):
-    username = os.getenv('MAKELAARSLAND_USERNAME', '')
-    password = os.getenv('MAKELAARSLAND_PASSWORD', '')
-    listing_id = os.getenv('MAKELAARSLAND_LISTING_ID', '')
+    username = os.getenv("MAKELAARSLAND_USERNAME", "")
+    password = os.getenv("MAKELAARSLAND_PASSWORD", "")
+    listing_id = os.getenv("MAKELAARSLAND_LISTING_ID", "")
 
     crawler = MakelaarslandCrawler(username, password, listing_id)
     crawler.crawl_listings(limit=limit)
 
 
 def crawl_walterliving():
-    proxy_url = os.getenv('PROXY_URL', '')
+    proxy_url = os.getenv("PROXY_URL", "")
     walter = WalterLiving(proxy_url)
-    walter.add_woz_values(csv_path='data/output/makelaarsland.csv')
+    walter.add_woz_values(csv_path="data/output/makelaarsland.csv")
 
 
 if __name__ == "__main__":
-    fire.Fire({
-        'makelaarsland': crawl_makelaarsland,
-        'walterliving': crawl_walterliving,
-    })
+    fire.Fire(
+        {"makelaarsland": crawl_makelaarsland, "walterliving": crawl_walterliving,}
+    )
--- src/makelaarsland/crawl.py
+++ src/makelaarsland/crawl.py
@@ -10,6 +10,8 @@ import requests
 from bs4 import BeautifulSoup
 from src.makelaarsland.makelaarsland import Makelaarsland
 
+CITY_REGEX = "((\d{4} \w{2}) (.*))"
+
 
 class MakelaarslandCrawler:
     BASE_URL = "https://mijn.makelaarsland.nl"
@@ -18,26 +20,23 @@ class MakelaarslandCrawler:
     SLEEP_TIMEOUT = 1
 
     HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:81.0) Gecko/20100101 Firefox/81.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'nl,en-US;q=0.7,en;q=0.3',
-        'Origin': 'https://mijn.makelaarsland.nl',
-        'Connection': 'keep-alive',
-        'Referer': 'https://mijn.makelaarsland.nl/Inloggen',
-        'Upgrade-Insecure-Requests': '1',
-        'Content-Type': 'application/x-www-form-urlencoded',
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:81.0) Gecko/20100101 Firefox/81.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "nl,en-US;q=0.7,en;q=0.3",
+        "Origin": "https://mijn.makelaarsland.nl",
+        "Connection": "keep-alive",
+        "Referer": "https://mijn.makelaarsland.nl/Inloggen",
+        "Upgrade-Insecure-Requests": "1",
+        "Content-Type": "application/x-www-form-urlencoded",
     }
-
-    listing_id: str = ''
-    session = None
-    makelaarsland = Makelaarsland()
 
     def __init__(self, username: str, password: str, listing_id: str):
         self.listing_id = listing_id
         self.session = self.login(username, password)
+        self.makelaarsland = Makelaarsland()
 
     def login(self, username, password) -> requests.Session:
-        """ Login to makelaarsland.nl and return a Session object.
+        """Login to makelaarsland.nl and return a Session object.
 
         Args:
             username (str)
@@ -52,9 +51,10 @@ class MakelaarslandCrawler:
         soup = BeautifulSoup(response.content, "html.parser")
         # get '__RequestVerificationToken' value
         req_verif_token = soup.find_all(
-            "input", {"name": "__RequestVerificationToken"})[0]['value']
+            "input", {"name": "__RequestVerificationToken"}
+        )[0]["value"]
 
-        payload = f'__RequestVerificationToken={req_verif_token}&MyAccount.Username={username}&MyAccount.Password={password}&RememberMe=false'
+        payload = f"__RequestVerificationToken={req_verif_token}&MyAccount.Username={username}&MyAccount.Password={password}&RememberMe=false"
 
         # login
         session.post(self.LOGIN_URL, data=payload, headers=self.HEADERS)
@@ -62,7 +62,7 @@ class MakelaarslandCrawler:
         return session
 
     def _get_listings_soup(self, listings_url) -> BeautifulSoup:
-        """ Return a BeautifulSoup object of the makelaarsland listings page.
+        """Return a BeautifulSoup object of the makelaarsland listings page.
 
         Args:
             listings_url ([type]): [description]
@@ -70,10 +70,15 @@ class MakelaarslandCrawler:
         Returns:
             BeautifulSoup: [description]
         """
-        print(f'Scraping {listings_url}')
+        print(f"Scraping {listings_url}")
         listings_response = self.session.get(listings_url)
-        listings_soup = BeautifulSoup(listings_response.content, features="html.parser")
-        return listings_soup
+        if listings_response and listings_response.content:
+            listings_soup = BeautifulSoup(
+                listings_response.content, features="html.parser"
+            )
+            return listings_soup
+        else:
+            raise Exception("Could not get listings soup")
 
     def _get_house_urls(self, listing: BeautifulSoup) -> List[str]:
         """Return all urls for the description of the house in the BeautifulSoup object.
@@ -84,12 +89,12 @@ class MakelaarslandCrawler:
         Returns:
             List[str]: [URLS of the houses]
         """
-        all_houses = listing.find_all(class_='house-content')
-        description_urls = [x.a['href'] for x in all_houses]
+        all_houses = listing.find_all(class_="house-content")
+        description_urls = [x.a["href"] for x in all_houses]
         return description_urls
 
     def _get_description(self, details_url) -> dict:
-        """ Extract the description from the house details URL.
+        """Extract the description from the house details URL.
 
         Args:
             details_url ([str]): [House description URL]
@@ -97,34 +102,40 @@ class MakelaarslandCrawler:
         Returns:
             dict: [Dictionary containing the description of the house]
         """
-        description_response = self.session.get(
-            f"{self.BASE_URL}{details_url}")
+        description_response = self.session.get(f"{self.BASE_URL}{details_url}")
         description_soup = BeautifulSoup(description_response.content)
 
         description_keys = description_soup.find_all(
-            'div', class_='col-12 col-sm-4 grey')
+            "div", class_="col-12 col-sm-4 grey"
+        )
         description_values = description_soup.find_all(
-            'div', class_='col-12 col-sm-8 darkgrey')
-        description_kv_pairs = {key.text.strip(): val.text.strip()
-                                for key, val in zip(description_keys, description_values)}
+            "div", class_="col-12 col-sm-8 darkgrey"
+        )
+        description_kv_pairs = {
+            key.text.strip(): val.text.strip()
+            for key, val in zip(description_keys, description_values)
+        }
 
-        description_kv_pairs['streetname'] = description_soup.find_all(
-            'div', class_='row justify-content-start mb-3')[0].text.strip()
+        description_kv_pairs["streetname"] = description_soup.find_all(
+            "div", class_="row justify-content-start mb-3"
+        )[0].text.strip()
 
         postal_code_str = description_soup.select(
-            '.house-sale > div:nth-child(3) > div:nth-child(1) > p:nth-child(1)')[0].text.strip()
-        description_kv_pairs['postal_code'] = re.search(
-            '((\d{4} \w{2}) (\w.*))', postal_code_str, re.IGNORECASE)[2]
-        description_kv_pairs['city'] = re.search(
-            '((\d{4} \w{2}) (\w.*))', postal_code_str, re.IGNORECASE)[3]
+            ".house-sale > div:nth-child(3) > div:nth-child(1) > p:nth-child(1)"
+        )[0].text.strip()
 
-        description_kv_pairs['url'] = f"{self.BASE_URL}{details_url}"
-        description_kv_pairs['date'] = f"{datetime.today().strftime('%d-%m-%y')}"
+        postal_code_match = re.search(CITY_REGEX, postal_code_str, re.IGNORECASE)
+        if postal_code_match:
+            description_kv_pairs["postal_code"] = postal_code_match[2]
+            description_kv_pairs["city"] = postal_code_match[3]
+
+        description_kv_pairs["url"] = f"{self.BASE_URL}{details_url}"
+        description_kv_pairs["date"] = f"{datetime.today().strftime('%d-%m-%y')}"
 
         return description_kv_pairs
 
     def _get_paginated_listing_urls(self, first_listing) -> List[str]:
-        """ Using the first listing page, find total number of listing pages, including paginated ones.
+        """Using the first listing page, find total number of listing pages, including paginated ones.
 
         Args:
             first_listing ([Beautifulsoup]): [BeautifulSoup object of the first listing page]
@@ -132,12 +143,12 @@ class MakelaarslandCrawler:
         Returns:
             List[str]: [List of all page URLs]
         """
-        pagination_list = first_listing.find_all('ul', {'class': 'pagination'})
+        pagination_list = first_listing.find_all("ul", {"class": "pagination"})
         listing_urls = []
         for pagination in pagination_list:
-            pagination_items = pagination.find_all('li')
+            pagination_items = pagination.find_all("li")
             for page in pagination_items:
-                listings_url = page.a.get('href')
+                listings_url = page.a.get("href")
                 listing_urls.append(f"{self.BASE_URL}{listings_url}")
 
         unique_urls = list(set(listing_urls))
@@ -145,15 +156,15 @@ class MakelaarslandCrawler:
         return page_urls
 
     def _check_logged_in(self, first_page_listing: BeautifulSoup):
-        not_logged_in = first_page_listing.find_all(text='Inloggen')
+        not_logged_in = first_page_listing.find_all(text="Inloggen")
         if not_logged_in:
-            print(f'Scrape failed. User not logged in.')
+            print(f"Scrape failed. User not logged in.")
             exit(1)
         else:
             print("Successfully logged in.")
 
     def _get_house_descriptions(self, limit: int = 0):
-        """ Retrieve the descriptions for the houses in the listing ID.
+        """Retrieve the descriptions for the houses in the listing ID.
 
         Args:
             limit ([int]): [Limit the number of crawled houses]
@@ -162,12 +173,13 @@ class MakelaarslandCrawler:
             [type]: [description]
         """
         first_page_listing = self._get_listings_soup(
-            self.FIRST_PAGE_LISTING_URL + self.listing_id)
+            self.FIRST_PAGE_LISTING_URL + self.listing_id
+        )
 
         self._check_logged_in(first_page_listing)
 
         # only take the limit of house urls to crawl
-        print(f'limit is={limit}')
+        print(f"limit is={limit}")
         if limit > 0:
             house_urls = self._get_house_urls(first_page_listing)
             if house_urls:
@@ -175,7 +187,7 @@ class MakelaarslandCrawler:
         else:
             house_urls = self._get_paginated_urls(first_page_listing)
 
-        print(f'Crawled listing {house_urls}')
+        print(f"Crawled listing {house_urls}")
 
         return self._crawl_house_descriptions(house_urls)
 
@@ -186,8 +198,7 @@ class MakelaarslandCrawler:
         for url in self._get_house_urls(first_page_listing):
             urls.append(url)
 
-        paginated_listing_urls = self._get_paginated_listing_urls(
-            first_page_listing)
+        paginated_listing_urls = self._get_paginated_listing_urls(first_page_listing)
 
         # add house description urls for the other pages
         for paginated_listing in paginated_listing_urls:
@@ -198,7 +209,7 @@ class MakelaarslandCrawler:
         return urls
 
     def _crawl_house_descriptions(self, urls: List[str]) -> List[str]:
-        """ Crawl the descriptions of the houses and create Listing objects.
+        """Crawl the descriptions of the houses and create Listing objects.
 
         Args:
             urls List[str]: List of URLs for the houses to crawl
@@ -210,34 +221,45 @@ class MakelaarslandCrawler:
         count = 0
 
         for url in urls:
+            if "woning-details" not in url:
+                print(f"Unknown URL {url}.. skipping.")
+                continue
+
             listing_dict = self._get_description(url)
             listing = self.makelaarsland.create_listing(
-                listing=listing_dict, to_dict=True)
+                listing=listing_dict, to_dict=True
+            )
             if listing:
                 listings.append(listing)
             time.sleep(self.SLEEP_TIMEOUT)
             print(
-                f'Crawled description number {count}/{len(urls)}: {self.BASE_URL}{url}')
+                f"Crawled description number {count}/{len(urls)}: {self.BASE_URL}{url}"
+            )
             count += 1
 
         return listings
 
     def _combine_results(self, input_dir: str, output_dir: str):
-        """ Combine the files in the input_dir to a single result in the output_dir.
+        """Combine the files in the input_dir to a single result in the output_dir.
 
         Args:
             input_dir (str): [Directory with the input files]
             output_dir (str): [Directory for the output file]
         """
-        df = pandas_read(input_dir, 'json')
-        df = df.drop_duplicates(subset=['streetname'])
+        df = pandas_read(input_dir, "json")
+        df = df.drop_duplicates(subset=["streetname"])
         df.reset_index(drop=True, inplace=True)
 
-        df.to_json(f'{output_dir}/makelaarsland.json', orient='records')
-        df.to_csv(f'{output_dir}/makelaarsland.csv')
+        df.to_json(f"{output_dir}/makelaarsland.json", orient="records")
+        df.to_csv(f"{output_dir}/makelaarsland.csv")
 
-    def crawl_listings(self, input_dir: str = 'data/scraped', output_dir: str = 'data/output', limit: int = 0):
-        """ Crawls the makelaarsland.nl website and creates a json file with the house descriptions.
+    def crawl_listings(
+        self,
+        input_dir: str = "data/scraped",
+        output_dir: str = "data/output",
+        limit: int = 0,
+    ):
+        """Crawls the makelaarsland.nl website and creates a json file with the house descriptions.
 
         Args:
             input_dir (str, optional): Location for the scraped files. Defaults to 'data/scraped'.
@@ -247,7 +269,7 @@ class MakelaarslandCrawler:
         descriptions = self._get_house_descriptions(limit)
 
         print(f"Finished crawling. Storing as JSON into dir: {input_dir}.")
-        current_date = datetime.today().strftime('%d-%m-%y')
+        current_date = datetime.today().strftime("%d-%m-%y")
 
         with open(f"{input_dir}/{current_date}.json", "w") as f:
             json.dump(descriptions, f)
